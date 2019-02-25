b825c731d2ba9e733dade11f2da0dbcf11ab935b
David Bolin <david@bolin.at>
2019-02-25 09:10:02 -0800

    completely finished rendering functionality

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 'use strict';
 
 const STORE = [
-  {name: 'apples', checked: false},
-  {name: 'oranges', checked: false},
-  {name: 'milk', checked: true},
-  {name: 'bread', checked: false},
+  {id: cuid(), name: 'apples', checked: false},
+  {id: cuid(), name: 'oranges', checked: false},
+  {id: cuid(), name: 'milk', checked: true},
+  {id: cuid(), name: 'bread', checked: false},
 ];
 
-function generateItemElement(item, itemIndex, template){
-  return `<li>${item.name}</li>`;
+function generateItemElement(item){
+  return `<li class="js-item-index-element" data-item-id="${item.id}">
+  <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle js-item-toggle">
+        <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete js-item-delete">
+        <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`;
 }
 
 function  generateShoppingItemsString(shoppingList){
-    const items= shoppingList.map((item, index) => generateItemElement(item, index));
-    console.log(items.join());
-    return items.join('');
+  const items= shoppingList.map((item, index) => generateItemElement(item, index));
+  return items.join('');
 }
 
 function renderShoppingList() {
   console.log('rendering shopping list');
   const shoppingListString = generateShoppingItemsString(STORE);
 
-  $(".js-shopping-list").html(shoppingListString);
+  $('.js-shopping-list').html(shoppingListString);
 
 }
 
