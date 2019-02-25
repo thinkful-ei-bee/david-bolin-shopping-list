e099a5f8fdf9576ddb5e92739c2381417e0bac2b
David Bolin <david@bolin.at>
2019-02-25 09:49:07 -0800

    added delete functionality

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ function renderShoppingList() {
 
 function addItemToShoppingList(itemName){
   console.log(`Adding "${itemName} to shopping list"`);
-  STORE.push({name: itemName, checked: false});
+  if (itemName !== '') {
+    STORE.push({id: cuid(), name: itemName, checked: false});
+  }
 }
 
 function handleNewItemSubmit() {
@@ -50,13 +52,41 @@ function handleNewItemSubmit() {
   });
   
 }
+function toggleCheckedForListItem(itemId) {
+  console.log(`Toggling checked property for item with with id ${itemId}.`);
+  const item = STORE.find(item => item.id === itemId);
+  item.checked = !item.checked;
+}
 
+function getItemIdFromElement(item) {
+  return $(item).closest('li').data('item-id');
+}
+
+function deleteItem(itemId) {
+  // const item = STORE.find(item => item.id === itemId);
+  const index = STORE.findIndex(item => item.id === itemId);
+  STORE.splice(index, 1);
+}
 function handleItemCheckClicked() {
   console.log('handling checked items');
+  $('.js-shopping-list').on('click', '.js-item-toggle', event => {
+    console.log('item clicked');
+    const itemId = getItemIdFromElement(event.currentTarget);
+    console.log(itemId);
+    toggleCheckedForListItem(itemId);
+    renderShoppingList();
+  });
 }
 
 function handleDeleteItemClicked() {
   console.log('handling deleted items');
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+    console.log('deleting item');
+    const itemId = getItemIdFromElement(event.currentTarget);
+    deleteItem(itemId);
+    renderShoppingList();
+  });
+
 }
 
 function main() {
